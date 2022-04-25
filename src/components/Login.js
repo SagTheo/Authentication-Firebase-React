9e3eaef2570d052fc47fefbd92fe23c0c6e63132
Theo <sagniez.theo@laposte.net>
2022-04-25 13:14:35 +0200

    Created forms + added some styles + worked on routing

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/form.css'
 
 const Login = () => {
   return (
-    <div>
+    <div className='container'>
         <h1>Log in</h1>
-        <form>
+        <form className='form'>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username"></input>
+            <input type="text" id="username" className='input'></input>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password"></input>
-            <button>Log in</button>
+            <input type="password" id="password" className='input'></input>
+            <button className='button'>Log in</button>
         </form>  
+        <p>Don't have an account ? <Link to='/signup'>Create one</Link></p>
     </div>
     
   )
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/form.css'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='container'>
+        <h1>Sign up</h1>
+        <form className='form'>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" className='input'></input>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" className='input'></input>
+            <label htmlFor="password">Confirm password</label>
+            <input type="password" id="password" className='input'></input>
+            <button className='button'>Sign up</button>
+        </form>  
+        <p>Already have an account ? <Link to='/'>Log in</Link></p>
+    </div>
   )
 }
 
