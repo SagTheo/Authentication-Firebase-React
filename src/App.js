68c046d987dbd6ff28bd3432c2e848f35fe2c57e
Theo <sagniez.theo@laposte.net>
2022-05-07 19:09:37 +0200

    Managed to restrict access to dashboard to signed in users only but logic needs more work

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Dashboard from './components/Dashboard'
-import PrivateRoute from './components/PrivateRoute'
+import NotFound from './components/NotFound'
 
 function App() {
+  const [user, setUser] = useState()
+
+  onAuthStateChanged(auth, user => {
+    setUser(user)
+  })
+
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={
-        <PrivateRoute>
-          <Dashboard />
-        </PrivateRoute>
-      } />
+      {
+        user && <Route path="/dashboard" element={<Dashboard />} />
+      }
+      <Route path="*" element={user ? <NotFound /> : <Login />} />
     </Routes>
   );
 }
